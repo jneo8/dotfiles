5c8f326afbc1d08ef38c8ef9463fa5c8240a1f29
jneo8 <jneo8lin@gmail.com>
2020-06-19 12:58:37 +0800

    Add README.md

---
+++ scripts/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+get_machine() {
+    unameOut="$(uname -s)"
+    case "${unameOut}" in
+        Linux*)     machine=Linux;;
+        Darwin*)    machine=Mac;;
+        CYGWIN*)    machine=Cygwin;;
+        MINGW*)     machine=MinGw;;
+        *)          machine="UNKNOWN:${unameOut}"
+    esac
+    echo ${machine}
+}
+
+echo $(get_machine)
