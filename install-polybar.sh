becd151b02d50116b21f7ca41fb4b73549830925
jneo8 <jneo8lin@gmail.com>
2022-05-19 00:51:53 +0800

    feat(polybar): Add laptop settings for polybar

--- install-polybar.sh
+++ install-polybar.sh
@@ -3,7 +3,7 @@
 # For compiling detail: https://github.com/polybar/polybar/wiki/Compiling#compiling
 
 cd /tmp && \
-    git clone --depth 1 --branch 3.5.4 --recursive https://github.com/polybar/polybar.git && \
+    git clone --depth 1 --branch 3.6.3 --recursive https://github.com/polybar/polybar.git && \
     cd /tmp/polybar && \
     pyenv local system && \
     # compile & install
